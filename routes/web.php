4b1e52a4af8207aded76c8d85bea63e29d564a71
Vinicius3545 <vinicius_oliveira_paschoal@hotmail.com>
2025-03-20 10:55:32 -0300

    estudo sobre rotas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/teste', function () {
+    return "É O SISTEMA";
+});
+
+Route::get('/soma/{num1}/{num2}', function ($num1, $num2) {
+    return "a soma é: ".$num1 + $num2;
+});
+
